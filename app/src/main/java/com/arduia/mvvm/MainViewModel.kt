e66b4955d2dd8d43beaaf5ceeac18f2ee935390c
Aung Ye Htet <agye.tech@gmail.com>
2020-11-17 21:40:57 +0630

    chore: add Rotation Sample

--- app/src/main/java/com/arduia/mvvm/MainViewModel.kt
+++ app/src/main/java/com/arduia/mvvm/MainViewModel.kt
@@ -1,19 +1,31 @@
 package com.arduia.mvvm
 
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel(), LifecycleObserver{
+class MainViewModel: ViewModel(){
 
-    private val _hello = BaseLiveData<String>()
-    val hello = _hello.asLiveData()
+    private val _isAvailable = BaseLiveData(initValue = true)
+    val isAvailable get() = _isAvailable.asLiveData()
 
+    private val _onRotate = EventLiveData<Unit>()
+    val onRotate get() = _onRotate.asLiveData()
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-    fun onCreate(){
-        _hello post "Hello World from MVVM Core Libraries"
+    private val _onToastShow = EventLiveData<String>()
+    val onToastShow get() = _onToastShow.asLiveData()
+
+    fun setAvailableOff(){
+        _isAvailable set false
     }
 
+    fun setAvailableOn(){
+        _isAvailable set true
+    }
+
+    fun rotate(){
+        _onRotate set EventUnit
+    }
+
+    fun showMessage(message: String){
+        _onToastShow set event(message)
+    }
 }
